c20af4c99c53f92b3b1a83cc52d2ed8e5595c661
Almir Abdrafikov <abdralmir@gmail.com>
2026-05-16 20:33:06 +0300

    Добавлен класс для покупателя и в БД добавлена таблица для хранения данных покупателей

---
+++ ComputerAccessoriesApp/Classes/Buyer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerAccessoriesApp.Classes
+{
+    internal class Buyer
+    {
+    }
+}
---
+++ ComputerAccessoriesApp/Migrations/20260516172227_AddBuyer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComputerAccessoriesApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuyer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "currrency_settings",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    currency_code = table.Column<string>(type: "text", nullable: false),
+                    exchange_rate = table.Column<decimal>(type: "numeric", nullable: false),
+                    updated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_currrency_settings", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: false),
+                    stock = table.Column<int>(type: "integer", nullable: false),
+                    unit = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    purchaseprice = table.Column<decimal>(type: "numeric", nullable: false),
+                    suppliedate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    validitymonths = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "shipments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    shipment_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    saleprice = table.Column<decimal>(type: "numeric", nullable: false),
+                    purchaseprice = table.Column<decimal>(type: "numeric", nullable: false),
+                    impactsum = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shipments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "supplies",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    purchase_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    supply_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    relevance_until = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_supplies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "currrency_settings");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "shipments");
+
+            migrationBuilder.DropTable(
+                name: "supplies");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
